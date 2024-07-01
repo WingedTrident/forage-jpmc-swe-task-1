16573f7cb5460117a3ee80da806f39870abb865e
WingedTrident <tbielwork@gmail.com>
2024-07-01 15:51:15 -0400

    Completed task

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0])[3], (120.48+121.2)/2)
+    self.assertEqual(getDataPoint(quotes[1])[3], (121.68+117.87)/2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,19 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertGreater(getDataPoint(quotes[0])[1],getDataPoint(quotes[0])[2])
+    self.assertGreater(getDataPoint(quotes[1])[1],getDataPoint(quotes[1])[2])
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_checkOutliers(self):
+    prices = [
+      {"ABC": 0, "DEF": 121.68},
+      {"ABC": 121.68, "DEF": 0}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    self.assertIs(getRatio(prices[0]["ABC"], prices[0]["DEF"]), 0.0)
+    self.assertIs(getRatio(prices[1]["ABC"], prices[1]["DEF"]), "Error: Dividing Price is Zero")
 
 
 
